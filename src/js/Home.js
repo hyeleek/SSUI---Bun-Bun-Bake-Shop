3588ed44674ecb6dff2cc2924d63f47f61a669c9
hailey822@naver.com <hailey822@naver.com>
2019-09-28 03:16:10 -0400

    Home Page

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
- 
+import { Link } from "react-router-dom";
+
+import title from "../assets/home.png";
+
 class Home extends Component {
   render() {
     return (
       <div>
-        <h2> Home </h2>
+        <img className="home-image" src={title}/>
+        <h1 className="home-title"> Bun Bun <br/> Bake Shop </h1>
+        <div className="intro">
+            <p>Since 1994, Bun Bun Bake Shop in Pittsburgh, Philadelphia, Gettysburg provide the perfect opportunity to experience Cinnamon Roll at its best. Treat yourself with a roll of Original Bun Bun Cinnamon Roll and a cup of coffee. Enjoy. </p>
+          <Link to="/About" style={{ textDecoration: 'none'}}> Learn More </Link>
+        </div>
+        <div className="order-button">
+          <Link to="/Order" style={{ textDecoration: 'none'}}> ORDER ONLINE</Link>
+        </div>
       </div>
     );
   }
 }
- 
+
 export default Home;
--- src/js/Main.js
+++ src/js/Main.js
@@ -18,43 +18,43 @@ class Main extends Component {
   render() {
     return (
       <HashRouter>
-        <div className="body">
-          <header>
-              <div className="nav-item">
-                <img src={Logo}/>
-                <NavLink exact to="/" className="nav-text home">
-                  Bun Bun Bake Shop
-                </NavLink>
-              </div>
-              <div className="nav-item">
-                <NavLink to="/About" className="nav-text">About</NavLink>
-              </div>
-              <div className="nav-item">
-                <NavLink to="/Order" className="nav-text">Order Online</NavLink>
-              </div>
-              <div className="nav-item">
-                <NavLink to="/Products" className="nav-text">Products</NavLink>
-              </div>
-              <div className="nav-item">
-                <NavLink to="/Contact" className="nav-text">Contact</NavLink>
-              </div>
-              <div className="nav-item">
-                <NavLink to="/Cart" className="nav-text">Cart</NavLink>
-              </div>
-          </header>
 
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/About" component={About}/>
-            <Route path="/Order" component={Order}/>
-            <Route path="/Products" component={Products}/>
-            <Route path="/Contact" component={Contact}/>
-            <Route path="/Cart" component={Cart}/>
-          </div>
+        <header>
+                <div className="nav-item">
+                  <img src={Logo}/>
+                  <NavLink exact to="/" className="nav-text home">
+                    Bun Bun Bake Shop
+                  </NavLink>
+                </div>
+                <div className="nav-item">
+                  <NavLink to="/About" className="nav-text">About</NavLink>
+                </div>
+                <div className="nav-item">
+                  <NavLink to="/Order" className="nav-text">Order Online</NavLink>
+                </div>
+                <div className="nav-item">
+                  <NavLink to="/Products" className="nav-text">Products</NavLink>
+                </div>
+                <div className="nav-item">
+                  <NavLink to="/Contact" className="nav-text">Contact</NavLink>
+                </div>
+                <div className="nav-item">
+                  <NavLink to="/Cart" className="nav-text">Cart</NavLink>
+                </div>
+            </header>
+
+
+        <div className="content">
+          <Route exact path="/" component={Home}/>
+          <Route path="/About" component={About}/>
+          <Route path="/Order" component={Order}/>
+          <Route path="/Products" component={Products}/>
+          <Route path="/Contact" component={Contact}/>
+          <Route path="/Cart" component={Cart}/>
         </div>
       </HashRouter>
     );
   }
 }
- 
+
 export default Main;
